a8377cdcad94aca7e227a37d50848763a148c639
abangkis <abangkis@aa2e8436-b5c8-11dd-bba1-afcbec1a1e1f>
2008-11-29 13:55:04 +0000

    some initial script

---
+++ repo/tip/trunk/init.sql
@@ -0,0 +1,14 @@
+
+insert into project
+(id, defaultproject, name, versions) 
+values (null, true, 'default project', '0.1');
+
+insert into tip.user
+(id, username, password, accessLevel, current_project_id) 
+values (null, 'abangkis', 'aa', 0, 1) 
+
+update user set current_project_id = 1;
+
+update user set access
+
+update taskitem set taskstatus = 0;
