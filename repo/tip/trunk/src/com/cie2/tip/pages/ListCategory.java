1250d601de2d7d2b9fd1cc80af156265373e8093
abangkis <abangkis@aa2e8436-b5c8-11dd-bba1-afcbec1a1e1f>
2008-12-25 03:33:02 +0000

    Adding parrent category in category list

--- repo/tip/trunk/src/com/cie2/tip/pages/ListCategory.java
+++ repo/tip/trunk/src/com/cie2/tip/pages/ListCategory.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.beaneditor.BeanModel;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.BeanModelSource;
 import org.hibernate.Session;
 
 import com.cie2.tip.components.base.CieUserPage;
 import com.cie2.tip.entities.Category;
+import com.cie2.tip.entities.TaskItem;
 
 public class ListCategory extends CieUserPage {
 	static Logger logger =
@@ -20,9 +24,21 @@ public class ListCategory extends CieUserPage {
 	@Inject
 	private Session session;
 	
+	@Inject
+	private BeanModelSource beanModelSource;
+
+	@Inject
+	private Messages messages;
+
 	
 	public List<Category> getCategories() {		
 		return session.createCriteria(Category.class).list();
 	}
 
+	public BeanModel getModel() {
+		BeanModel model = beanModelSource.create(Category.class, false,
+				messages);
+		model.add("parentCategory", null);
+		return model;
+	}	
 }
